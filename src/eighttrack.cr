8d4e06baf978fcc07882ebe672a701598356af38
Holden Omans <holden.omans@gmail.com>
2018-08-10 19:09:43 -0400

    refactor to allow multiple requests

--- src/eighttrack.cr
+++ src/eighttrack.cr
@@ -2,19 +2,27 @@ require "habitat"
 require "./eighttrack/*"
 
 module EightTrack
+  @@sequence = 0
+
   Habitat.create do
     setting tape_library_dir : String = "spec/fixtures/eighttracks"
   end
 
-  @@channel = Channel(String).new
+  # The name of the tape
+  def self.tape_name
+    @@tape_name
+  end
 
-  def self.channel
-    @@channel
+  # The current sequence, calling this will increment the value
+  def self.sequence
+    @@sequence += 1
   end
 
   def self.use_tape(tape_name : String, &block)
-    spawn { EightTrack.channel.send(tape_name) }
+    @@tape_name = tape_name
+    @@sequence = 0
     block.call
+    @@tape_name = nil
   end
 end
 
--- src/eighttrack/core_ext.cr
+++ src/eighttrack/core_ext.cr
@@ -1,10 +1,40 @@
 require "http/client"
+require "digest"
+
+class HTTP::Request
+  def to_json()
+    {
+      method: method,
+      host: host,
+      resource: resource,
+      headers: headers.to_h,
+      body: body.to_s,
+      query_params: query_params.to_h,
+    }.to_json()
+  end
+end
 
 class HTTP::Client
   private def exec_internal(request : HTTP::Request)
-    tape_name = EightTrack.channel.receive
-    tape_path = EightTrack.settings.tape_library_dir + "/#{tape_name}"
+    tape_name = EightTrack.tape_name
 
+    # If we do not have a tape_name, perform a normal request
+    if(tape_name.nil?)
+      request.to_io(socket)
+      socket.flush
+      return HTTP::Client::Response.from_io(socket, request.ignore_body?)
+    end
+
+    # Create an md5 for the request
+    req_md5 = Digest::MD5.hexdigest(request.to_json)
+
+    # Create path vars
+    tape_dir = File.join(EightTrack.settings.tape_library_dir, tape_name)
+    # Create a dir for our tape
+    FileUtils.mkdir(tape_dir) unless(Dir.exists?(tape_dir))
+    tape_path = File.join(tape_dir, "#{EightTrack.sequence}.#{req_md5}.vcr")
+
+    # If it exists, load and return the data
     if File.exists?(tape_path)
       HTTP::Client::Response.from_io(File.open(tape_path))
     else
