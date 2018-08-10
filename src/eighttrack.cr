852c0bbe53a1f1bcce0a21d0bcaf79d24fea926a
Holden Omans <holden.omans@gmail.com>
2018-08-10 19:36:55 -0400

    added record macro

--- src/eighttrack.cr
+++ src/eighttrack.cr
@@ -3,6 +3,7 @@ require "./eighttrack/*"
 
 module EightTrack
   @@sequence = 0
+  @@record = false
 
   Habitat.create do
     setting tape_library_dir : String = "spec/fixtures/eighttracks"
@@ -21,8 +22,33 @@ module EightTrack
   def self.use_tape(tape_name : String, &block)
     @@tape_name = tape_name
     @@sequence = 0
+
+    reset_casset(tape_name) if record?
+
     block.call
+    reset!
+  end
+
+  def self.record=(value)
+    @@record = value
+  end
+
+  def self.record?
+    @@record
+  end
+
+  private def self.reset!
     @@tape_name = nil
+    @@record = false
+  end
+
+  private def self.reset_casset(casset)
+    dir = File.join(EightTrack.settings.tape_library_dir, casset)
+    Dir.open(dir).each do |file|
+      if file =~ /\.vcr$/
+        File.delete(File.join(dir, file))
+      end
+    end
   end
 end
 
