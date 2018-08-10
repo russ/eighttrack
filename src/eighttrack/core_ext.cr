2aea31213fb09f8e366338c0967eecb8f1f86d8a
Holden Omans <holden.omans@gmail.com>
2018-08-10 19:23:50 -0400

    formatting

--- src/eighttrack/core_ext.cr
+++ src/eighttrack/core_ext.cr
@@ -2,15 +2,15 @@ require "http/client"
 require "digest"
 
 class HTTP::Request
-  def to_json()
+  def to_json
     {
-      method: method,
-      host: host,
-      resource: resource,
-      headers: headers.to_h,
-      body: body.to_s,
+      method:       method,
+      host:         host,
+      resource:     resource,
+      headers:      headers.to_h,
+      body:         body.to_s,
       query_params: query_params.to_h,
-    }.to_json()
+    }.to_json
   end
 end
 
@@ -19,7 +19,7 @@ class HTTP::Client
     tape_name = EightTrack.tape_name
 
     # If we do not have a tape_name, perform a normal request
-    if(tape_name.nil?)
+    if (tape_name.nil?)
       request.to_io(socket)
       socket.flush
       return HTTP::Client::Response.from_io(socket, request.ignore_body?)
@@ -31,7 +31,7 @@ class HTTP::Client
     # Create path vars
     tape_dir = File.join(EightTrack.settings.tape_library_dir, tape_name)
     # Create a dir for our tape
-    FileUtils.mkdir(tape_dir) unless(Dir.exists?(tape_dir))
+    FileUtils.mkdir(tape_dir) unless (Dir.exists?(tape_dir))
     tape_path = File.join(tape_dir, "#{EightTrack.sequence}.#{req_md5}.vcr")
 
     # If it exists, load and return the data
